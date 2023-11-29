a0fd0a044ad3e1610ec77095947bbf9822cca4b4
Gabrielle <gabrielle.castro@ymonetize.com>
2023-11-29 15:28:04 -0300

    at1

--- src/app/Sobre/page.tsx
+++ src/app/Sobre/page.tsx
@@ -4,10 +4,40 @@ import Header from '../components/header/page'
 
 export default function Sobre() {
   return (
-    <main className={styles.container}>
+    <div className={styles.container}>
       <Header />
 
+      <main className={styles.content}>
+        <div className={styles.boxHeader}>
+          <div>
+            <h1>Quem Somos</h1>
+            <p>Conheça um pouco mais sobre a Jubanorpa</p>
+          </div>
+        </div>
 
-    </main>
+        <div className={styles.boxInfo}>
+          <text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nullam vitae augue suscipit neque tincidunt vulputate eget eu metus.</text>
+
+          <h2>Nosso propósito:</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nullam vitae augue suscipit neque tincidunt vulputate eget eu metus.
+            Proin auctor libero semper sem venenatis imperdiet. Maecenas elit lorem,
+            gravida a sapien nec, placerat vulputate urna. Suspendisse eu porttitor
+            risus. Proin gravida facilisis velit, in consequat nisi commodo vitae.</p>
+
+          <h2>Nossa visão:</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nullam vitae augue suscipit neque tincidunt vulputate eget eu metus.
+            Proin auctor libero semper sem venenatis imperdiet. Maecenas elit lorem,
+            gravida a sapien nec, placerat vulputate urna. Suspendisse eu porttitor
+            risus. Proin gravida facilisis velit, in consequat nisi commodo vitae.
+            <br /><br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nullam vitae augue suscipit neque tincidunt vulputate eget eu metus.
+          </p>
+        </div>
+      </main>
+    </div>
   )
 }
